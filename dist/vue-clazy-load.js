1ff949a9799f52142f38904635266c090713ef2b
Le Minh Tri <ansidev@gmail.com>
2019-01-04 02:05:33 +0700

    Update dependencies and use only yarn.lock

--- dist/vue-clazy-load.js
+++ dist/vue-clazy-load.js
@@ -7,7 +7,7 @@
 		exports["VueClazyLoad"] = factory();
 	else
 		root["VueClazyLoad"] = factory();
-})(typeof self !== 'undefined' ? self : this, function() {
+})(window, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -46,12 +46,32 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
 /******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -69,6 +89,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
@@ -78,7 +99,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+__webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "install", function() { return install; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "VueClazyLoad", function() { return VueClazyLoad; });
 /*!
@@ -99,6 +120,7 @@ var ClazyLoadComponent = {
       type: String,
       default: 'div'
     },
+
     /**
      * Image source URL
      * @type {String}
@@ -108,11 +130,13 @@ var ClazyLoadComponent = {
       type: String,
       required: true
     },
+
     /**
      * IntersectionObserver root element
      * @type {String}
      */
     element: String,
+
     /**
      * IntersectionObserver threshold
      * @type {Array, Number}
@@ -123,6 +147,7 @@ var ClazyLoadComponent = {
         return [0, 0.5, 1];
       }
     },
+
     /**
      * InserectionObserver visibility ratio
      * @type {Number}
@@ -135,6 +160,7 @@ var ClazyLoadComponent = {
         return value > 0 && value <= 1;
       }
     },
+
     /**
      * IntersectionObserver root margin
      * @type {String}
@@ -143,6 +169,7 @@ var ClazyLoadComponent = {
       type: String,
       default: '0px'
     },
+
     /**
      * Optional CORS mode ("anonymous" | "use-credentials")
      * @see https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
@@ -156,6 +183,7 @@ var ClazyLoadComponent = {
         return value === 'anonymous' || value === 'use-credentials';
       }
     },
+
     /**
      * Class added to element when it finishes loading
      * @type {String}
@@ -165,6 +193,7 @@ var ClazyLoadComponent = {
       type: String,
       default: 'loaded'
     },
+
     /**
      * Class added to element while it is loading
      * @type {String}
@@ -173,6 +202,7 @@ var ClazyLoadComponent = {
       type: String,
       default: 'loading'
     },
+
     /**
      * Class added to element if loading failed
      * @type {String}
@@ -189,7 +219,6 @@ var ClazyLoadComponent = {
       errored: false
     };
   },
-
   methods: {
     /**
      * Start loading image
@@ -198,42 +227,38 @@ var ClazyLoadComponent = {
       var _this = this;
 
       // emits 'loading' event upwards
-      this.$emit('loading');
-
-      // disconnect observer
+      this.$emit('loading'); // disconnect observer
       // so it doesn't load more than once
+
       this.observer.disconnect();
 
       if (!this.loaded) {
         // fake image
         var img = new Image();
-
         img.addEventListener('load', function () {
-          _this.loaded = true;
-          // emits 'load' event upwards
+          _this.loaded = true; // emits 'load' event upwards
+
           _this.$emit('load');
 
-          _clear();
+          clear();
         });
-
         img.addEventListener('error', function (event) {
-          _this.errored = true;
-          // emits 'error' event upwards
+          _this.errored = true; // emits 'error' event upwards
           // adds the original event as argument
+
           _this.$emit('error', event);
 
-          _clear();
-        });
+          clear();
+        }); // function used to clear variables from memory
 
-        // function used to clear variables from memory
-        var _clear = function _clear() {
+        var clear = function clear() {
           // discard fake image
-          img = null;
-          // remove observer from memory
-          _this.observer = null;
-        };
+          img = null; // remove observer from memory
 
-        // CORS mode configuration
+          _this.observer = null;
+        }; // CORS mode configuration
+
+
         if (this.crossorigin !== null) {
           img.crossOrigin = this.crossorigin;
         }
@@ -241,7 +266,6 @@ var ClazyLoadComponent = {
         img.src = this.src;
       }
     },
-
 
     /**
      * Creates IntersectionObserver instance and observe current element
@@ -252,25 +276,23 @@ var ClazyLoadComponent = {
       var options = {
         threshold: this.threshold,
         root: this.element ? document.querySelector(this.element) : null,
-        rootMargin: this.margin
+        rootMargin: this.margin // creates IO instance
 
-        // creates IO instance
-      };this.observer = new IntersectionObserver(function (entries) {
+      };
+      this.observer = new IntersectionObserver(function (entries) {
         // as we instantiated one for each component
         // we can directly access the first index
         if (entries[0].intersectionRatio >= _this2.ratio) {
           _this2.load();
         }
-      }, options);
+      }, options); // start observing main component
 
-      // start observing main component
       this.observer.observe(this.$el);
     }
   },
   render: function render(h) {
     // class to be added to element indicating load state
     var elementClass = this.loaded ? this.loadedClass : this.loadingClass;
-
     return h(this.tag, {
       // if loading failed adds error class if exists,
       // otherwhise adds elementClass defined above
@@ -282,17 +304,14 @@ var ClazyLoadComponent = {
     // start observing the element visibility
     this.$nextTick(this.observe);
   }
-};
+}; // Export install function for CDN embeds
 
-// Export install function for CDN embeds
 var install = function install(Vue) {
   Vue.component('clazy-load', ClazyLoadComponent);
-};
+}; // Component object
 
-// Component object
-var VueClazyLoad = ClazyLoadComponent;
+var VueClazyLoad = ClazyLoadComponent; // Exports default object for ES6 modules
 
-// Exports default object for ES6 modules
 /* harmony default export */ __webpack_exports__["default"] = ({
   install: install
 });
